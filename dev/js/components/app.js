00c204efe44d2fc4a09d630158ede817e0e3c483
Salva Carsi <salvavcarsi@gmail.com>
2017-01-03 18:08:01 +0100

    redux tutorial 11 user detail component

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import UserList from '../containers/user-list';
+import UserDetail from '../containers/user-detail'
 require('../../scss/style.scss');
 
 const App = () => (
   <div>
     <h2>Username List:</h2>
-    <UserList/>
+    <UserList />
     <br />
     <h2>User Details:</h2>
+    <UserDetail />
   </div>
 );
 
